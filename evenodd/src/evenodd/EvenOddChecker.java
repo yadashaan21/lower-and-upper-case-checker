ba8b7ce409a5d7ff7043782d728eae3d2823c9a0
B P YADAV <shaanyadav160@gmail.com>
2025-02-04 00:04:27 +0530

    second commit

---
+++ evenodd/src/evenodd/EvenOddChecker.java
@@ -0,0 +1,24 @@
+package evenodd;
+
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        // Create a Scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even.");
+        } else {
+            System.out.println(number + " is Odd.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
